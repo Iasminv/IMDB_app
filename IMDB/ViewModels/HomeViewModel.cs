454f18e30f6784698ff30f5516ac3165ce6827f4
MasonLiao319 <alienml@yahoo.com>
2025-04-14 16:28:06 -0300

    Several Changes has been made

--- IMDB/ViewModels/HomeViewModel.cs
+++ IMDB/ViewModels/HomeViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using IMDB.Commands;
 using IMDB.Services;
 using IMDB.Views;
+using IMDB_App.Data;
 using IMDB_App.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Windows.Input;
 
@@ -15,11 +17,19 @@ namespace IMDB.ViewModels
         private string _searchText;
         private ObservableCollection<Title> _searchResults = new ObservableCollection<Title>();
         private bool _isLoading;
+        private bool _isSearched;
 
         public string SearchText
         {
             get => _searchText;
-            set => SetProperty(ref _searchText, value);
+            set
+            {
+                if (SetProperty(ref _searchText, value))
+                {
+                    
+                    PerformDynamicSearch();
+                }
+            }
         }
 
         public ObservableCollection<Title> SearchResults
@@ -34,6 +44,12 @@ namespace IMDB.ViewModels
             set => SetProperty(ref _isLoading, value);
         }
 
+        public bool IsSearched
+        {
+            get => _isSearched;
+            set => SetProperty(ref _isSearched, value);
+        }
+
         public ICommand SearchCommand { get; }
         public ICommand NavigateToMoviesCommand { get; }
         public ICommand NavigateToTVShowsCommand { get; }
@@ -46,23 +62,49 @@ namespace IMDB.ViewModels
             _navigationService = navigationService;
 
             SearchCommand = new RelayCommand(Search);
-            NavigateToMoviesCommand = new RelayCommand(() => NavigateToMovies("movie"));
-            NavigateToTVShowsCommand = new RelayCommand(() => NavigateToMovies("tvSeries"));
+            NavigateToMoviesCommand = new RelayCommand(() => NavigateToMoviesList("movie"));
+            NavigateToTVShowsCommand = new RelayCommand(() => NavigateToMoviesList("tvSeries"));
             NavigateToActorsCommand = new RelayCommand(NavigateToActors);
             NavigateToGenresCommand = new RelayCommand(NavigateToGenres);
             SelectTitleCommand = new RelayCommand<Title>(SelectTitle);
+        }
 
-            // Data is pre-loaded by App.LoadData()
+        private void PerformDynamicSearch()
+        {
+            if (string.IsNullOrWhiteSpace(SearchText) || SearchText.Length < 2)
+            {
+                // Clear results if search text is empty 
+                SearchResults.Clear();
+                IsSearched = false;
+                return;
+            }
+
+            IsLoading = true;
+            IsSearched = true;
+
+            using (var context = new ImdbContext())
+            {
+                var titles = context.Titles
+                    .Where(t => t.PrimaryTitle.Contains(SearchText))
+                    .Include(t => t.Rating)
+                    .Take(10)  // Limit to 10 for dynamic search to be responsive
+                    .ToList();
+
+                SearchResults = new ObservableCollection<Title>(titles);
+            }
+
+            IsLoading = false;
         }
 
         private void Search()
         {
+            
             if (string.IsNullOrWhiteSpace(SearchText))
                 return;
 
             IsLoading = true;
 
-            using (var context = new IMDB_App.Data.ImdbContext())
+            using (var context = new ImdbContext())
             {
                 var titles = context.Titles
                     .Where(t => t.PrimaryTitle.Contains(SearchText))
@@ -73,12 +115,13 @@ namespace IMDB.ViewModels
                 SearchResults = new ObservableCollection<Title>(titles);
             }
 
+            IsSearched = true;
             IsLoading = false;
         }
 
-        private void NavigateToMovies(string titleType)
+        private void NavigateToMoviesList(string titleType)
         {
-            _navigationService.NavigateToWithViewModel<MovieDetailsView, MovieDetailsViewModel>(titleType);
+            _navigationService.NavigateToWithViewModel<MovieListView, MovieListViewModel>(titleType);
         }
 
         private void NavigateToActors()
--- IMDB/ViewModels/MovieListViewModel.cs
+++ IMDB/ViewModels/MovieListViewModel.cs
@@ -13,9 +13,11 @@ namespace IMDB.ViewModels
     public class MovieListViewModel : ViewModelBase, IParameterizedViewModel
     {
         private readonly INavigationService _navigationService;
-        private int _genreId;
         private ObservableCollection<Title> _titles = new ObservableCollection<Title>();
-        private string _genreName;
+        private string _listTitle;
+        private string _searchText;
+        private bool _isLoading;
+        private object _parameter; // Store the passed parameter (genre ID or titleType)
 
         public ObservableCollection<Title> Titles
         {
@@ -23,44 +25,71 @@ namespace IMDB.ViewModels
             set => SetProperty(ref _titles, value);
         }
 
-        public string GenreName
+        public string ListTitle
         {
-            get => _genreName;
-            set => SetProperty(ref _genreName, value);
+            get => _listTitle;
+            set => SetProperty(ref _listTitle, value);
         }
 
-        public ICommand BackToGenresCommand { get; }
+        public string SearchText
+        {
+            get => _searchText;
+            set
+            {
+                if (SetProperty(ref _searchText, value))
+                {
+                   
+                    PerformDynamicSearch();
+                }
+            }
+        }
+
+        public bool IsLoading
+        {
+            get => _isLoading;
+            set => SetProperty(ref _isLoading, value);
+        }
+
+        public ICommand BackCommand { get; }
         public ICommand SelectTitleCommand { get; }
+        public ICommand SearchCommand { get; }
 
         public MovieListViewModel(INavigationService navigationService)
         {
             _navigationService = navigationService;
-            BackToGenresCommand = new RelayCommand(NavigateToGenres);
+            BackCommand = new RelayCommand(NavigateBack);
             SelectTitleCommand = new RelayCommand<Title>(SelectTitle);
+            SearchCommand = new RelayCommand(Search);
         }
 
         public void Initialize(object parameter)
         {
+            _parameter = parameter;
+
             if (parameter is int genreId)
             {
-                _genreId = genreId;
-                LoadGenreMovies();
+                LoadGenreMovies(genreId);
+            }
+            else if (parameter is string titleTypeParam)
+            {
+                LoadTitlesByType(titleTypeParam);
             }
         }
 
-        private void LoadGenreMovies()
+        private void LoadGenreMovies(int genreId)
         {
+            IsLoading = true;
             using (var context = new ImdbContext())
             {
-                var genre = context.Genres.FirstOrDefault(g => g.GenreId == _genreId);
+                var genre = context.Genres.FirstOrDefault(g => g.GenreId == genreId);
                 if (genre != null)
                 {
-                    GenreName = genre.Name;
+                    ListTitle = $"{genre.Name} Movies & Shows";
                 }
 
                 // Get movies for this genre
                 var titlesQuery = context.Titles
-                    .Where(t => t.Genres.Any(g => g.GenreId == _genreId))
+                    .Where(t => t.Genres.Any(g => g.GenreId == genreId))
                     .Include(t => t.Rating)
                     .OrderByDescending(t => t.Rating.AverageRating)
                     .Take(50)
@@ -68,6 +97,91 @@ namespace IMDB.ViewModels
 
                 Titles = new ObservableCollection<Title>(titlesQuery);
             }
+            IsLoading = false;
+        }
+
+        private void LoadTitlesByType(string titleType)
+        {
+            IsLoading = true;
+
+            switch (titleType)
+            {
+                case "movie":
+                    ListTitle = "Movies";
+                    break;
+                case "tvSeries":
+                    ListTitle = "TV Shows";
+                    break;
+                default:
+                    ListTitle = "Titles";
+                    break;
+            }
+
+            using (var context = new ImdbContext())
+            {
+                var titlesQuery = context.Titles
+                    .Where(t => t.TitleType == titleType)
+                    .Include(t => t.Rating)
+                    .OrderByDescending(t => t.Rating.AverageRating)
+                    .Take(50)
+                    .ToList();
+
+                Titles = new ObservableCollection<Title>(titlesQuery);
+            }
+            IsLoading = false;
+        }
+
+        private void PerformDynamicSearch()
+        {
+            if (string.IsNullOrWhiteSpace(SearchText) || SearchText.Length < 2)
+            {
+                // If search text is empty or too short, reload original data
+                if (_parameter is int genreId)
+                {
+                    LoadGenreMovies(genreId);
+                }
+                else if (_parameter is string titleType)
+                {
+                    LoadTitlesByType(titleType);
+                }
+                return;
+            }
+
+            IsLoading = true;
+
+            using (var context = new ImdbContext())
+            {
+                IQueryable<Title> query = context.Titles.Include(t => t.Rating);
+
+                // If filtering by type
+                if (_parameter is string titleType)
+                {
+                    query = query.Where(t => t.TitleType == titleType);
+                }
+                // If filtering by genre
+                else if (_parameter is int genreId)
+                {
+                    query = query.Where(t => t.Genres.Any(g => g.GenreId == genreId));
+                }
+
+                // Add search condition
+                query = query.Where(t => t.PrimaryTitle.Contains(SearchText));
+
+                // Sort and get results
+                var results = query.OrderByDescending(t => t.Rating.AverageRating)
+                                   .Take(50)
+                                   .ToList();
+
+                Titles = new ObservableCollection<Title>(results);
+            }
+
+            IsLoading = false;
+        }
+
+        private void Search()
+        {
+            
+            PerformDynamicSearch();
         }
 
         private void SelectTitle(Title title)
@@ -76,9 +190,16 @@ namespace IMDB.ViewModels
             _navigationService.NavigateToWithViewModel<MovieDetailsView, MovieDetailsViewModel>(title.TitleId);
         }
 
-        private void NavigateToGenres()
+        private void NavigateBack()
         {
-            _navigationService.NavigateToWithViewModel<GenresView, GenresViewModel>();
+            if (_parameter is int)
+            {
+                _navigationService.NavigateToWithViewModel<GenresView, GenresViewModel>();
+            }
+            else
+            {
+                _navigationService.NavigateToWithViewModel<HomeView, HomeViewModel>();
+            }
         }
     }
 }
